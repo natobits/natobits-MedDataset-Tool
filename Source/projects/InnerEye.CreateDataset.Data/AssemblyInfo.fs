507a6e620b8deba41ff442c7b844f6e14a534f7c
natobits <natobits@proton.me>
2023-10-29 07:30:10 +0000

    minor code formatting

--- Source/projects/InnerEye.CreateDataset.Data/AssemblyInfo.fs
+++ Source/projects/InnerEye.CreateDataset.Data/AssemblyInfo.fs
@@ -37,4 +37,10 @@ open System.Runtime.InteropServices
 //       Revision
 // 
 // You can specify all the values or you can default the Build and Revision Numbers 
-// by usi
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
