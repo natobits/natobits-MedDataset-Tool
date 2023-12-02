e10fd577b77dc381fb6a05c2290604b30d35b141
natobits <natobits@proton.me>
2023-12-02 12:10:55 +0000

    Server

--- Source/projects/InnerEye.CreateDataset.Math/VolumeExtensions/TemplatedExtensions.cs
+++ Source/projects/InnerEye.CreateDataset.Math/VolumeExtensions/TemplatedExtensions.cs
@@ -1384,4 +1384,263 @@ namespace InnerEye.CreateDataset.Math
                 }
             });
 
-            var region = new Region3D<int>(minimumX.Min(), minimumY.Min(), minimumZ.Min(), maximumX.Max(), maximumY.Max(), maxi
+            var region = new Region3D<int>(minimumX.Min(), minimumY.Min(), minimumZ.Min(), maximumX.Max(), maximumY.Max(), maximumZ.Max());
+            
+			// If no foreground values are found, the region minimum will be Int.MaxValue, maximum will be Int.MinValue.
+			// When accidentally doing operations on that region, it will most likely lead to numerical
+			// overflow or underflow. Instead, return an empty region that has less troublesome boundary values.
+			return region.IsEmpty() ? RegionExtensions.EmptyIntRegion() : region;
+        }
+
+		/// <summary>
+		/// Returns the maximum and minimum of the array.
+		/// Throws an ArgumentNullException if the array is null.
+		/// Throws an ArgumentException if the array has no element.
+		/// </summary>
+		public static MinMax<float> GetMinMax(this float[] array)
+		{
+			if (array == null)
+			{
+				throw new ArgumentNullException(nameof(array));
+			}
+
+			if (array.Length == 0)
+			{
+				throw new ArgumentException("The array must have non-zero length");
+			}
+
+			var minValue = float.MaxValue;			
+			var maxValue = float.MinValue;
+			foreach (var currentValue in array)
+			{
+				if (currentValue < minValue)
+				{
+					minValue = currentValue;
+				}
+
+				if (currentValue > maxValue)
+				{
+					maxValue = currentValue;
+				}
+			}
+
+			return new MinMax<float>() { Maximum = maxValue, Minimum = minValue };
+		}
+
+		/// <summary>
+		/// Returns the difference between maximum and minimum in the given range.
+		/// </summary>
+		public static float Range(this MinMax<float> minMax)
+		{
+			if (minMax.Minimum > minMax.Maximum)
+			{
+				throw new ArgumentException("The minimum must not be larger than the maximum", nameof(minMax));
+			}
+
+			return (float)(minMax.Maximum - minMax.Minimum);
+		}
+
+		/// <summary>
+		/// If the given value falls inside the range stored in <paramref name="minMax"/>, return the
+		/// value unchanged. If the value is smaller than the range minimum, return the minimum.
+		/// If the value is larger than the range maximum, return the maximum.
+		/// </summary>
+		public static float Clamp(this MinMax<float> minMax, float value)
+		{
+			if (value < minMax.Minimum)
+			{
+				return minMax.Minimum;
+			}
+
+			if (value > minMax.Maximum)
+			{
+				return minMax.Maximum;
+			}
+
+			return value;
+		}
+
+		/// <summary>
+		/// Returns the minimum and maximum voxel value in the volume.
+		/// Throws an ArgumentNullException if the volume is null.
+		/// </summary>
+		public static MinMax<float> GetMinMax(this Volume<float> volume)
+		{
+			if (volume == null)
+			{
+				throw new ArgumentNullException(nameof(volume));
+			}
+
+			return volume.Array.GetMinMax();
+		}
+
+		/// <summary>
+		/// Returns the minimum of the array.
+		/// Throws an ArgumentNullException if the array is null.
+		/// Throws an ArgumentException if the array has no element.
+		/// </summary>
+		public static float Minimum(this float[] array)
+		{
+			if (array == null)
+			{
+				throw new ArgumentNullException(nameof(array));
+			}
+
+			if (array.Length == 0)
+			{
+				throw new ArgumentException("The array must have non-zero length");
+			}
+
+			var minValue = float.MaxValue;
+			foreach (var currentValue in array)
+			{
+				if (currentValue < minValue)
+				{
+					minValue = currentValue;
+				}
+			}
+
+			return minValue;
+		}
+
+		/// <summary>
+		/// Returns the maximum of the array.
+		/// Throws an ArgumentNullException if the array is null.
+		/// Throws an ArgumentException if the array has no element.
+		/// </summary>
+		public static float Maximum(this float[] array)
+		{
+			if (array == null)
+			{
+				throw new ArgumentNullException(nameof(array));
+			}
+
+			if (array.Length == 0)
+			{
+				throw new ArgumentException("The array must have non-zero length");
+			}
+
+			var maxValue = float.MinValue;
+			foreach (var currentValue in array)
+			{
+				if (currentValue > maxValue)
+				{
+					maxValue = currentValue;
+				}
+			}
+
+			return maxValue;
+		}
+
+        /// <summary>
+        /// Computes the minimum and the maximum of a sequence of values, in a single pass through the data.
+        /// Returns null if the input sequence is empty.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static MinMax<float>? GetRange(this IEnumerable<float> stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            var max = float.MinValue;
+            var min = float.MaxValue;
+            var any = false;
+            foreach (var d in stream)
+            {
+                any = true;
+                if (d > max)
+                {
+                    max = d;
+                }
+
+                if (d < min)
+                {
+                    min = d;
+                }
+            }
+
+            if (!any)
+            {
+                return null;
+            }
+
+            return MinMax.Create(min, max);
+        }
+
+        /// <summary>
+        /// Checks whether all of the voxel values in the input volume are inside of the given <see cref="range"/>.
+        /// If a voxel value is below the given minimum, it is changed to be exactly at the minimum.
+        /// If a voxel value is above the given maximum, it is changed to be exactly at the maximum.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="range"></param>
+        public static void ClipToRangeInPlace(this Volume3D<float> image, MinMax<float> range)
+        {
+            if (image == null)
+            {
+                throw new ArgumentNullException(nameof(image));
+            }
+
+			if (range.Minimum > range.Maximum)
+			{
+				throw new ArgumentException($"Invalid range provided: Minimum = {range.Minimum}, Maximum = {range.Maximum}", nameof(range));
+			}
+
+            var array = image.Array;
+            for (var index = 0; index < array.Length; index++)
+            {
+                var value = array[index];
+                if (value < range.Minimum)
+                {
+                    array[index] = range.Minimum;
+                }
+                else if (value > range.Maximum)
+                {
+                    array[index] = range.Maximum;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a binary mask by thresholding the present volume. All voxels that have
+        /// a value larger or equal than the threshold will be set to 1
+		/// (<see cref="ModelConstants.MaskForegroundIntensity">) in the result,
+		/// all other voxels will be set to 0 (<see cref="ModelConstants.MaskBackgroundIntensity">)
+		/// </summary>
+        /// <param name="geoDistanceByte">A volume that stores geodesic distances.</param>
+        /// <param name="geoThreshold">The minimum distance that is required for a voxel to be considered foreground.</param>
+        /// <returns></returns>
+        public static Volume3D<byte> Threshold(
+            this Volume3D<float> volume, 
+            float threshold)
+        {
+            return volume.Map(value =>
+                value >= threshold
+                ? ModelConstants.MaskForegroundIntensity
+                : ModelConstants.MaskBackgroundIntensity);
+        }
+
+	        /// <summary>
+        /// Gets the region of the volume that contains all voxel values that are
+		/// larger or equal than the interestId.
+        /// </summary>
+        /// <param name="volume"></param>
+        /// <param name="interestId">The voxel values to search for. Foreground is
+		/// considered to be all voxels with a value larger or equal to the interestId.</param>
+        /// <returns></returns>
+		public static Region3D<int> GetInterestRegion(this Volume3D<byte> volume, byte interestId)
+        {
+            var minimumX = new int[volume.DimZ];
+            var minimumY = new int[volume.DimZ];
+            var minimumZ = new int[volume.DimZ];
+            var maximumX = new int[volume.DimZ];
+            var maximumY = new int[volume.DimZ];
+            var maximumZ = new int[volume.DimZ];
+
+            Parallel.For(0, volume.DimZ, delegate (int z)
+            {
+                minimumX[z] = int.MaxValue;
+                minimum
