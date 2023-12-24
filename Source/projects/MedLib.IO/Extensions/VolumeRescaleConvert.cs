7ce085e8aee5e4667c6ab22a545a13d8cdd16aa2
natobits <natobits@proton.me>
2023-12-24 12:18:19 +0000

    add docker

--- Source/projects/MedLib.IO/Extensions/VolumeRescaleConvert.cs
+++ Source/projects/MedLib.IO/Extensions/VolumeRescaleConvert.cs
@@ -190,4 +190,206 @@
 					for (; pDestPtr <= pDestEnd; pDestPtr++, pSrcPtr++)
 					{
 						var v = *pSrcPtr * slope + intercept;
-						int
+						int vi = (int)Math.Round(v);
+						*pDestPtr =(short)(vi < short.MinValue ? short.MinValue : (vi > short.MaxValue ? short.MaxValue : vi));
+					}
+				}
+			};
+		}
+
+		/// <summary>
+		/// Returns an action to convert an array of bytes encoding a contiguous array of type float to an array of type short applying slope and intercept
+		/// to the given values. All values are clamped to the range short.MinValue and short.MaxValue as appropriate.
+		/// </summary>
+		public unsafe static Action<int, int> Convertfloat(byte[] srcBytes, short[] output, float slope, float intercept)
+		{
+			return (startIndex, endIndex) =>
+			{
+				fixed (byte* pSrc = srcBytes)
+				fixed (short* pDest = output)
+				{
+					short* pDestEnd = pDest + endIndex;
+					short* pDestPtr = pDest + startIndex;
+#pragma warning disable IDE0004 // Remove Unnecessary Cast
+					float* pSrcPtr = (float*)pSrc + startIndex;
+#pragma warning restore IDE0004 // Remove Unnecessary Cast
+					for (; pDestPtr <= pDestEnd; pDestPtr++, pSrcPtr++)
+					{
+						var v = *pSrcPtr * slope + intercept;
+						int vi = (int)Math.Round(v);
+						*pDestPtr =(short)(vi < short.MinValue ? short.MinValue : (vi > short.MaxValue ? short.MaxValue : vi));
+					}
+				}
+			};
+		}
+
+        /// <summary>
+        /// Returns an action to convert an array of bytes encoding a contiguous array of type byte to an array of type ushort applying slope and intercept
+        /// to the given values. All values are clamped to the range ushort.MinValue and ushort.MaxValue as appropriate.
+        /// </summary>
+        public unsafe static Action<int, int> Convertbyte(byte[] srcBytes, ushort[] output, float slope, float intercept)
+		{
+			return (startIndex, endIndex) =>
+			{
+				fixed (byte* pSrc = srcBytes)
+				fixed (ushort* pDest = output)
+				{
+					ushort* pDestEnd = pDest + endIndex;
+					ushort* pDestPtr = pDest + startIndex;
+#pragma warning disable IDE0004 // Remove Unnecessary Cast
+					byte* pSrcPtr = (byte*)pSrc + startIndex;
+#pragma warning restore IDE0004 // Remove Unnecessary Cast
+					for (; pDestPtr <= pDestEnd; pDestPtr++, pSrcPtr++)
+					{
+						var v = *pSrcPtr * slope + intercept;
+						int vi = (int)Math.Round(v);
+						*pDestPtr =(ushort)(vi < ushort.MinValue ? ushort.MinValue : (vi > ushort.MaxValue ? ushort.MaxValue : vi));
+					}
+				}
+			};
+		}
+
+		/// <summary>
+		/// Returns an action to convert an array of bytes encoding a contiguous array of type short to an array of type ushort applying slope and intercept
+		/// to the given values. All values are clamped to the range ushort.MinValue and ushort.MaxValue as appropriate.
+		/// </summary>
+		public unsafe static Action<int, int> Convertshort(byte[] srcBytes, ushort[] output, float slope, float intercept)
+		{
+			return (startIndex, endIndex) =>
+			{
+				fixed (byte* pSrc = srcBytes)
+				fixed (ushort* pDest = output)
+				{
+					ushort* pDestEnd = pDest + endIndex;
+					ushort* pDestPtr = pDest + startIndex;
+#pragma warning disable IDE0004 // Remove Unnecessary Cast
+					short* pSrcPtr = (short*)pSrc + startIndex;
+#pragma warning restore IDE0004 // Remove Unnecessary Cast
+					for (; pDestPtr <= pDestEnd; pDestPtr++, pSrcPtr++)
+					{
+						var v = *pSrcPtr * slope + intercept;
+						int vi = (int)Math.Round(v);
+						*pDestPtr =(ushort)(vi < ushort.MinValue ? ushort.MinValue : (vi > ushort.MaxValue ? ushort.MaxValue : vi));
+					}
+				}
+			};
+		}
+
+		/// <summary>
+		/// Returns an action to convert an array of bytes encoding a contiguous array of type ushort to an array of type ushort applying slope and intercept
+		/// to the given values. All values are clamped to the range ushort.MinValue and ushort.MaxValue as appropriate.
+		/// </summary>
+		public unsafe static Action<int, int> Convertushort(byte[] srcBytes, ushort[] output, float slope, float intercept)
+		{
+			return (startIndex, endIndex) =>
+			{
+				fixed (byte* pSrc = srcBytes)
+				fixed (ushort* pDest = output)
+				{
+					ushort* pDestEnd = pDest + endIndex;
+					ushort* pDestPtr = pDest + startIndex;
+#pragma warning disable IDE0004 // Remove Unnecessary Cast
+					ushort* pSrcPtr = (ushort*)pSrc + startIndex;
+#pragma warning restore IDE0004 // Remove Unnecessary Cast
+					for (; pDestPtr <= pDestEnd; pDestPtr++, pSrcPtr++)
+					{
+						var v = *pSrcPtr * slope + intercept;
+						int vi = (int)Math.Round(v);
+						*pDestPtr =(ushort)(vi < ushort.MinValue ? ushort.MinValue : (vi > ushort.MaxValue ? ushort.MaxValue : vi));
+					}
+				}
+			};
+		}
+
+		/// <summary>
+		/// Returns an action to convert an array of bytes encoding a contiguous array of type float to an array of type ushort applying slope and intercept
+		/// to the given values. All values are clamped to the range ushort.MinValue and ushort.MaxValue as appropriate.
+		/// </summary>
+		public unsafe static Action<int, int> Convertfloat(byte[] srcBytes, ushort[] output, float slope, float intercept)
+		{
+			return (startIndex, endIndex) =>
+			{
+				fixed (byte* pSrc = srcBytes)
+				fixed (ushort* pDest = output)
+				{
+					ushort* pDestEnd = pDest + endIndex;
+					ushort* pDestPtr = pDest + startIndex;
+#pragma warning disable IDE0004 // Remove Unnecessary Cast
+					float* pSrcPtr = (float*)pSrc + startIndex;
+#pragma warning restore IDE0004 // Remove Unnecessary Cast
+					for (; pDestPtr <= pDestEnd; pDestPtr++, pSrcPtr++)
+					{
+						var v = *pSrcPtr * slope + intercept;
+						int vi = (int)Math.Round(v);
+						*pDestPtr =(ushort)(vi < ushort.MinValue ? ushort.MinValue : (vi > ushort.MaxValue ? ushort.MaxValue : vi));
+					}
+				}
+			};
+		}
+
+		/// <summary>
+		/// Returns an action to convert an array of bytes encoding a contiguous array of type byte to an array of type float applying slope and intercept
+		/// to the given values. All values are clamped to the range float.MinValue and float.MaxValue as appropriate.
+		/// </summary>
+		public unsafe static Action<int, int> Convertbyte(byte[] srcBytes, float[] output, float slope, float intercept)
+		{
+			return (startIndex, endIndex) =>
+			{
+				fixed (byte* pSrc = srcBytes)
+				fixed (float* pDest = output)
+				{
+					float* pDestEnd = pDest + endIndex;
+					float* pDestPtr = pDest + startIndex;
+#pragma warning disable IDE0004 // Remove Unnecessary Cast
+					byte* pSrcPtr = (byte*)pSrc + startIndex;
+#pragma warning restore IDE0004 // Remove Unnecessary Cast
+					for (; pDestPtr <= pDestEnd; pDestPtr++, pSrcPtr++)
+					{
+						var v = *pSrcPtr * slope + intercept;
+						float vi = (v);
+						*pDestPtr =vi;
+					}
+				}
+			};
+		}
+
+		/// <summary>
+		/// Returns an action to convert an array of bytes encoding a contiguous array of type short to an array of type float applying slope and intercept
+		/// to the given values. All values are clamped to the range float.MinValue and float.MaxValue as appropriate.
+		/// </summary>
+		public unsafe static Action<int, int> Convertshort(byte[] srcBytes, float[] output, float slope, float intercept)
+		{
+			return (startIndex, endIndex) =>
+			{
+				fixed (byte* pSrc = srcBytes)
+				fixed (float* pDest = output)
+				{
+					float* pDestEnd = pDest + endIndex;
+					float* pDestPtr = pDest + startIndex;
+#pragma warning disable IDE0004 // Remove Unnecessary Cast
+					short* pSrcPtr = (short*)pSrc + startIndex;
+#pragma warning restore IDE0004 // Remove Unnecessary Cast
+					for (; pDestPtr <= pDestEnd; pDestPtr++, pSrcPtr++)
+					{
+						var v = *pSrcPtr * slope + intercept;
+						float vi = (v);
+						*pDestPtr =vi;
+					}
+				}
+			};
+		}
+
+		/// <summary>
+		/// Returns an action to convert an array of bytes encoding a contiguous array of type ushort to an array of type float applying slope and intercept
+		/// to the given values. All values are clamped to the range float.MinValue and float.MaxValue as appropriate.
+		/// </summary>
+		public unsafe static Action<int, int> Convertushort(byte[] srcBytes, float[] output, float slope, float intercept)
+		{
+			return (startIndex, endIndex) =>
+			{
+				fixed (byte* pSrc = srcBytes)
+				fixed (float* pDest = output)
+				{
+					float* pDestEnd = pDest + endIndex;
+					float* pDestPtr = pDest + startIndex;
+#pragma warning disable IDE0004 // Remove Unnecessary Cast
