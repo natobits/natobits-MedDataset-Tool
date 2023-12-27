74fc1d6632bd6721725e56fa1375f969669df575
natobits <natobits@proton.me>
2023-12-27 11:57:20 +0000

    Updates to the XUL stuff

--- Source/projects/MedLib.IO/NiiToDicomHelpers.cs
+++ Source/projects/MedLib.IO/NiiToDicomHelpers.cs
@@ -82,4 +82,137 @@
             string studyId = null,
             string patientName = null)
         {
-            if (!IsVali
+            if (!IsValidDicomLongString(manufacturer))
+            {
+                throw new ArgumentException("The manufacturer is not a valid Dicom Long String.", nameof(manufacturer));
+            }
+
+            if (!IsValidDicomLongString(patientName))
+            {
+                throw new ArgumentException("The patient name is not a valid Dicom Long String.", nameof(patientName));
+            }
+
+            return new DicomDataset()
+            {
+                { new DicomUniqueIdentifier(DicomTag.Manufacturer, manufacturer ?? string.Empty) },
+                { new DicomUniqueIdentifier(DicomTag.StudyID, studyId ?? string.Empty) },
+                { new DicomUniqueIdentifier(DicomTag.PatientName, patientName ?? string.Empty) }
+            };
+        }
+
+        /// <summary>
+        /// Creates a Dicom UID for internal use only.
+        /// The reason why we are not using fo-dicom uid creation code is because it is very slow.
+        /// </summary>
+        /// <returns></returns>
+        public static DicomUID CreateUID()
+        {
+            return new DicomUID(GuidToUidStringUsingStringAndParse(Guid.NewGuid()), string.Empty, DicomUidType.Unknown);
+        }
+
+        /// <summary>
+        /// Converts a volume 3D into a collection of Dicom files (split by slice on the primary plane).
+        /// This code writes the patient position as HFS (this might not be correct but was needed at some point to view the output).
+        /// 
+        /// Note: This code has not been tested with MR data. It also assumes the Photometric Interpretation to be MONOCHROME2.
+        /// Use with extreme care - many Dicom elements have to be halluzinated here, and there's no 
+        /// guarantee that the resulting Dicom will be usable beyond what is needed in InnerEye.
+        /// </summary>
+        /// <param name="volume">The volume to convert.</param>
+        /// <param name="modality">The image modality.</param>
+        /// <param name="seriesDescription">The value to use as the Dicom series description.</param>
+        /// <param name="patientID">The patient ID that should be used in the Dicom files. If null,
+        /// a randomly generated patient ID will be used.</param>
+        /// <param name="studyInstanceID">The study ID that should be used in the Dicom files (DicomTag.StudyInstanceUID). If null,
+        /// a randomly generated study ID will be used.</param>
+        /// <param name="additionalDicomItems">Additional Dicom items that will be added to each of the slice datasets. This can
+        /// be used to pass in additional information like manufacturer.</param>
+        /// <returns>The collection of Dicom files that represents the Dicom image series.</returns>
+        public static IEnumerable<DicomFile> Convert(Volume3D<short> volume,
+            ImageModality modality,
+            string seriesDescription = null,
+            string patientID = null,
+            string studyInstanceID = null,
+            DicomDataset additionalDicomItems = null)
+        {
+            seriesDescription = seriesDescription ?? string.Empty;
+            patientID = CreateUidIfEmpty(patientID);
+            studyInstanceID = CreateUidIfEmpty(studyInstanceID);
+
+            if (!IsValidDicomLongString(seriesDescription))
+            {
+                throw new ArgumentException("The series description is not a valid Dicom Long String.", nameof(seriesDescription));
+            }
+
+            if (!IsValidDicomLongString(patientID))
+            {
+                throw new ArgumentException("The patient ID is not a valid Dicom Long String.", nameof(patientID));
+            }
+
+            if (!IsValidDicomLongString(studyInstanceID))
+            {
+                throw new ArgumentException("The study instance ID is not a valid Dicom Long String.", nameof(studyInstanceID));
+            }
+
+            var spacingZ = volume.SpacingZ;
+            var imageOrientationPatient = new decimal[6];
+
+            var directionColumn1 = volume.Direction.Column(0);
+            var directionColumn2 = volume.Direction.Column(1);
+
+            imageOrientationPatient[0] = (decimal)directionColumn1.X;
+            imageOrientationPatient[1] = (decimal)directionColumn1.Y;
+            imageOrientationPatient[2] = (decimal)directionColumn1.Z;
+            imageOrientationPatient[3] = (decimal)directionColumn2.X;
+            imageOrientationPatient[4] = (decimal)directionColumn2.Y;
+            imageOrientationPatient[5] = (decimal)directionColumn2.Z;
+
+            var frameOfReferenceUID = CreateUID().UID;
+            var seriesUID = CreateUID().UID;
+            var sopInstanceUIDs = new DicomUID[volume.DimZ];
+
+            // DicomUID.Generate() is not thread safe. We must create unique DicomUID's single threaded.
+            // https://github.com/fo-dicom/fo-dicom/issues/546
+            for (var i = 0; i < sopInstanceUIDs.Length; i++)
+            {
+                sopInstanceUIDs[i] = CreateUID();
+            }
+
+            var results = new DicomFile[volume.DimZ];
+            Parallel.For(0, volume.DimZ, i =>
+            {
+                var sliceLocation = (i * spacingZ) + volume.Origin.Z;
+                var imagePositionPatient = volume.Transform.DataToDicom.Transform(new Point3D(0, 0, i));
+
+                var dataset = new DicomDataset()
+                {
+                    { DicomTag.ImageType, new[] {"DERIVED", "PRIMARY", "AXIAL" } },
+                    { DicomTag.PatientPosition, "HFS" },
+                    { new DicomOtherWord(DicomTag.PixelData, new MemoryByteBuffer(ExtractSliceAsByteArray(volume, i))) },
+                    { new DicomUniqueIdentifier(DicomTag.SOPInstanceUID, sopInstanceUIDs[i]) },
+                    { new DicomUniqueIdentifier(DicomTag.SeriesInstanceUID, seriesUID) },
+                    { new DicomUniqueIdentifier(DicomTag.PatientID, patientID) },
+                    { new DicomUniqueIdentifier(DicomTag.StudyInstanceUID, studyInstanceID) },
+                    { new DicomUniqueIdentifier(DicomTag.FrameOfReferenceUID, frameOfReferenceUID) },
+                    { new DicomLongString(DicomTag.SeriesDescription, seriesDescription) },
+                    { new DicomUnsignedShort(DicomTag.Columns, (ushort)volume.DimX) },
+                    { new DicomUnsignedShort(DicomTag.Rows, (ushort)volume.DimY) },
+                    { new DicomDecimalString(DicomTag.PixelSpacing, (decimal)volume.SpacingY, (decimal)volume.SpacingX) }, // Note: Spacing X & Y are not the expected way around
+                    { new DicomDecimalString(DicomTag.ImagePositionPatient, (decimal)imagePositionPatient.X, (decimal)imagePositionPatient.Y, (decimal)imagePositionPatient.Z) },
+                    { new DicomDecimalString(DicomTag.ImageOrientationPatient, imageOrientationPatient) },
+                    { new DicomDecimalString(DicomTag.SliceLocation, (decimal)sliceLocation) },
+                    { new DicomUnsignedShort(DicomTag.SamplesPerPixel, DicomSeriesInformationValidator.ExpectedSamplesPerPixel) },
+                    { new DicomUnsignedShort(DicomTag.PixelRepresentation, 1) },
+                    { new DicomUnsignedShort(DicomTag.BitsStored, DicomSeriesInformationValidator.ExpectedBitsAllocated) },
+                    { new DicomUnsignedShort(DicomTag.BitsAllocated, DicomSeriesInformationValidator.ExpectedBitsAllocated) },
+                    { new DicomUnsignedShort(DicomTag.HighBit, DicomSeriesInformationValidator.ExpectedBitsAllocated - 1) },
+                    { new DicomCodeString(DicomTag.PhotometricInterpretation, DicomSeriesInformationValidator.ExpectedPhotometricInterpretation) }
+                };
+
+                if (modality == ImageModality.CT)
+                {
+                    dataset.Add(DicomTag.SOPClassUID, DicomUID.CTImageStorage);
+                    dataset.Add(DicomTag.Modality, ImageModality.CT.ToString());
+
+                    dataset.Add(new DicomItem[]
+                
